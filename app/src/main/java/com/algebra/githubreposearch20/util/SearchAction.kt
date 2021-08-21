232eb14eee0a259a96fd2453ca12b5379d89fc75
Tomislav Vujevic <tomi.vujevic@gmail.com>
2021-08-21 02:48:49 +0200

    Add swipe refresh, clear cache

--- app/src/main/java/com/algebra/githubreposearch20/util/SearchAction.kt
+++ app/src/main/java/com/algebra/githubreposearch20/util/SearchAction.kt
@@ -4,16 +4,18 @@ import android.content.Context
 import androidx.appcompat.widget.SearchView
 import com.algebra.githubreposearch20.App
 import com.algebra.githubreposearch20.R
+import com.algebra.githubreposearch20.presentation.ui.helper.RefreshHelper
 import com.algebra.githubreposearch20.presentation.ui.viewmodel.GitHubRepoViewModel
 import kotlinx.coroutines.*
 
 var queryTextChangedJob: Job? = null
 
-fun searchAction(searchView: SearchView, context: Context, viewModel: GitHubRepoViewModel) {
+fun searchAction(searchView: SearchView, context: Context, viewModel: GitHubRepoViewModel, refreshHelper: RefreshHelper) {
     searchView.setOnQueryTextListener(
         object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 query?.let { search ->
+                    refreshHelper.searchResult = search
                     viewModel.getAllGitHubRepos(search)
                 } ?: displayMessage(App.getStringResource(R.string.unexpected_error), context)
                 return false
@@ -27,9 +29,11 @@ fun searchAction(searchView: SearchView, context: Context, viewModel: GitHubRepo
                     newText?.let { search ->
                         when {
                             search.length >= 3 -> {
+                                refreshHelper.searchResult = search
                                 viewModel.getAllGitHubRepos(search)
                                 searchView.clearFocus()
                             }
+                            else -> refreshHelper.searchResult = search
                         }
                     }
                 }
